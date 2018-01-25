e030373b28ee8f0c4da09406122cd4ce015edbac
jcprice12 <jcprice12@gmail.com>
2018-01-24 22:08:25 -0500

    started adding db functionality

---
+++ src/main/java/com/john/price/PetAdoption/Repositories/PetRepository.java
@@ -0,0 +1,9 @@
+package com.john.price.PetAdoption.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.john.price.PetAdoption.Models.Pet;
+
+public interface PetRepository extends CrudRepository<Pet, Integer> {
+
+}
