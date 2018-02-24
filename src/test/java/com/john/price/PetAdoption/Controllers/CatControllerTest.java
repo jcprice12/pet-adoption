086c58a941c91b22d4634a647f47555462d68c03
jcprice12 <jcprice12@gmail.com>
2018-02-24 16:08:13 -0500

    moved data.sql to test folder. tests now have seed data. started writing tests for controllers and endpoints

---
+++ src/test/java/com/john/price/PetAdoption/Controllers/CatControllerTest.java
@@ -0,0 +1,11 @@
+package com.john.price.PetAdoption.Controllers;
+
+import org.junit.Test;
+
+public class CatControllerTest {
+	
+	@Test
+	public void allShouldReturnAllCats() throws Exception{
+		
+	}
+}
