b728d3c6c9c3b0514d723e158a26503b0f936e36
jcprice12 <john.c.price12@gmail.com>
2019-02-14 15:16:04 -0500

    made an abstract service class for pets with breeds

--- server/src/main/java/com/john/price/PetAdoption/Services/CatService.java
+++ server/src/main/java/com/john/price/PetAdoption/Services/CatService.java
@@ -1,63 +1,40 @@
 package com.john.price.PetAdoption.Services;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.john.price.PetAdoption.Functions.CatToCatWithoutCatsInBreedsMapper;
-import com.john.price.PetAdoption.Models.Breed;
+import com.john.price.PetAdoption.Functions.PetToPetMapper;
 import com.john.price.PetAdoption.Models.Cat;
 import com.john.price.PetAdoption.Repositories.CatRepository;
 
 @Component
-public class CatService implements PetService<Cat>{
+public class CatService extends PetWithBreedsService<Cat>{
 	
 	@Autowired
 	private CatToCatWithoutCatsInBreedsMapper catToCatWithoutCatsInBreedsMapper;
 	
 	@Autowired
 	private CatRepository catRepository;
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	private void insertIntoIntersectionTable(Cat cat) {
-		List<Object[]> parameters = new ArrayList<Object[]>();
-	    for (Breed breed : cat.getBreeds()) {
-	        parameters.add(new Object[] {breed.getId(), cat.getId()});
-	    }
-		jdbcTemplate.batchUpdate("INSERT INTO catbreed_cat (breed_id, cat_id) VALUES (?, ?)", parameters);
+
+	@Override
+	protected PetToPetMapper<Cat, Cat> getPetToPetMapper() {
+		return catToCatWithoutCatsInBreedsMapper;
 	}
 
 	@Override
-	public Collection<Cat> getPets() {		
-		return catRepository.findAll().stream().map(catToCatWithoutCatsInBreedsMapper).collect(Collectors.toList());
+	protected JpaRepository<Cat, Integer> getRepository() {
+		return catRepository;
 	}
 
 	@Override
-	public Cat getPet(Integer id) {
-		return catToCatWithoutCatsInBreedsMapper.apply(catRepository.findOne(id));
+	protected String getIntersectionTableName() {
+		return "catbreed_cat";
 	}
-	
+
 	@Override
-	@Transactional(propagation = Propagation.NESTED)
-	public Cat createPet(Cat cat) {
-		Cat savedCat = catRepository.save(cat);
-		insertIntoIntersectionTable(savedCat);
-		return savedCat;
-	}
-	
-	@Override
-	@Transactional
-	public Cat editPet(Cat cat) {
-		jdbcTemplate.update("DELETE from catbreed_cat where cat_id = ?", cat.getId());
-		return createPet(cat);
+	protected String getPetIdColumnName() {
+		return "cat_id";
 	}
 }
--- server/src/main/java/com/john/price/PetAdoption/Services/DogService.java
+++ server/src/main/java/com/john/price/PetAdoption/Services/DogService.java
@@ -1,23 +1,16 @@
 package com.john.price.PetAdoption.Services;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.john.price.PetAdoption.Functions.DogToDogWithoutDogsInBreedsMapper;
-import com.john.price.PetAdoption.Models.Breed;
+import com.john.price.PetAdoption.Functions.PetToPetMapper;
 import com.john.price.PetAdoption.Models.Dog;
 import com.john.price.PetAdoption.Repositories.DogRepository;
 
 @Component
-public class DogService implements PetService<Dog> {
+public class DogService extends PetWithBreedsService<Dog> {
 	
 	@Autowired
 	private DogToDogWithoutDogsInBreedsMapper dogToDogWithoutDogsInBreedsMapper;
@@ -25,39 +18,23 @@ public class DogService implements PetService<Dog> {
 	@Autowired
 	private DogRepository dogRepository;
 	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	private void insertIntoIntersectionTable(Dog dog) {
-		List<Object[]> parameters = new ArrayList<Object[]>();
-	    for (Breed breed : dog.getBreeds()) {
-	        parameters.add(new Object[] {breed.getId(), dog.getId()});
-	    }
-		jdbcTemplate.batchUpdate("INSERT INTO dogbreed_dog (breed_id, dog_id) VALUES (?, ?)", parameters);
-	}
-	
 	@Override
-	public Collection<Dog> getPets() {
-		return dogRepository.findAll().stream().map(dogToDogWithoutDogsInBreedsMapper).collect(Collectors.toList());
+	protected PetToPetMapper<Dog, Dog> getPetToPetMapper() {
+		return dogToDogWithoutDogsInBreedsMapper;
 	}
 
 	@Override
-	public Dog getPet(Integer id) {
-		return dogToDogWithoutDogsInBreedsMapper.apply(dogRepository.findOne(id));
+	protected JpaRepository<Dog, Integer> getRepository() {
+		return dogRepository;
 	}
 
 	@Override
-	@Transactional(propagation = Propagation.NESTED)
-	public Dog createPet(Dog dog) {
-		Dog savedDog = dogRepository.save(dog);
-		insertIntoIntersectionTable(savedDog);
-		return savedDog;
+	protected String getIntersectionTableName() {
+		return "dogbreed_dog";
 	}
 
 	@Override
-	@Transactional
-	public Dog editPet(Dog dog) {
-		jdbcTemplate.update("DELETE from dogbreed_dog where dog_id = ?", dog.getId());
-		return createPet(dog);
+	protected String getPetIdColumnName() {
+		return "dog_id";
 	}
 }
