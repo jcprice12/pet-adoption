c56cd53cc35ca6401ada0429660e230fd5521c2e
jcprice12 <john.c.price12@gmail.com>
2024-09-14 18:27:30 -0400

    local s3 service

--- src/main/java/com/john/price/PetAdoption/Services/LocalS3Service.java
+++ src/main/java/com/john/price/PetAdoption/Services/LocalS3Service.java
@@ -1,6 +1,13 @@
 package com.john.price.PetAdoption.Services;
 
+import java.io.File;
+import java.net.InetAddress;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,9 +17,22 @@ import com.john.price.PetAdoption.Exceptions.UploadS3FileException;
 @Profile("dev")
 public class LocalS3Service implements IS3Service {
 
+  @Autowired
+  Environment environment;
+
   @Override
   public String uploadMultipartFileToS3(MultipartFile multipartFile, String keyPrefix) throws UploadS3FileException {
-    String fname = multipartFile.getOriginalFilename();
-    return fname;
+    try {
+      String originalFileName = multipartFile.getOriginalFilename();
+      String filePath = String.format("local-images/%s/%d_%s", keyPrefix, System.currentTimeMillis(), originalFileName);
+      Path absolutePath = Paths.get(System.getProperty("user.dir"), "src/main/resources/static", filePath);
+      File file = absolutePath.toFile();
+      file.getParentFile().mkdirs();
+      multipartFile.transferTo(file);
+      return String.format("%s:%s/%s", InetAddress.getLoopbackAddress().getHostName(),
+          environment.getProperty("local.server.port"), filePath);
+    } catch (Exception e) {
+      throw new UploadS3FileException(e);
+    }
   }
 }
