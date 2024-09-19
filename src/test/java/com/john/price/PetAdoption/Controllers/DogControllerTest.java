96cd92d2804437fc8ed848af35f9fe4b4d4738c0
jcprice12 <john.c.price12@gmail.com>
2024-09-18 22:01:54 -0400

    spring boot 2.6.0

--- src/test/java/com/john/price/PetAdoption/Controllers/DogControllerTest.java
+++ src/test/java/com/john/price/PetAdoption/Controllers/DogControllerTest.java
@@ -1,8 +1,8 @@
 package com.john.price.PetAdoption.Controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
