ecda36f695b00b043fba8a90924908f39729ba77
jcprice12 <john.c.price12@gmail.com>
2024-09-24 21:24:11 -0400

    updated tests and added maven wrapper

--- src/test/java/com/john/price/PetAdoption/Models/DogBreedTest.java
+++ src/test/java/com/john/price/PetAdoption/Models/DogBreedTest.java
@@ -1,15 +1,15 @@
 package com.john.price.PetAdoption.Models;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class DogBreedTest {
 
     DogBreed dogBreed;
 
-    @Before
+    @BeforeEach
     public void beforeEachTest() {
         dogBreed = new DogBreed();
     }
